6c68a27ece5190e4a192109540b627d9168f9d7e
Christopher <christopher.ridolfi@greenwoodcollege.com>
2018-10-30 09:46:16 -0400

    commit compare()

--- Function Tasks.py
+++ Function Tasks.py
@@ -8,4 +8,14 @@ def product():
 print(product())
 
 
+def compare():
+    com_1 = int(input("Input a number"))
+    com_2 = int(input("input a second number"))
+    total_2 = com_1 = com_2
+    if total_2 == True:
+        print(True)
+    else:
+        print("False")
 
+
+print(compare())
