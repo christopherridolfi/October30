7ac547a9be2cae9ccaddffe1f49a840b25760479
Christopher <christopher.ridolfi@greenwoodcollege.com>
2018-11-07 11:49:21 -0500

    Lesson 2

--- List Day 2.py
+++ List Day 2.py
@@ -2,3 +2,15 @@ weekdays = ["sun", "Tues", "Wed", "Thurs", "Fri"]
 print(weekdays)
 del weekdays[0]
 print(weekdays)
+weekdays.insert(0,"Mon")
+print(weekdays)
+
+
+a = "hello World"
+b = "42"
+c = 42
+
+
+print(a.isdigit())
+print(b.isdigit())
+print(c.isdigit()) # doesn't work because function only works for strings and the number is not a string (not in quotes)
