97e185f824430222001261caded87ecd1e069aba
Christopher <christopher.ridolfi@greenwoodcollege.com>
2018-11-08 13:04:49 -0500

    Part 1

---
+++ List/2D Lists.py
@@ -0,0 +1,11 @@
+a = [1,2,3]
+b = [99,98,97]
+c = [7,8,9]
+newlist = [a,b,c]
+print(newlist[1][1])
+
+for x in newlist:
+    print(x)
+
+string_list = ["hello", "World"]
+print(string_list[1][2])
---
+++ List/Lists and Strings.py
@@ -0,0 +1,56 @@
+import random
+
+list_1 = ["Hi", "Hello", "Holo", "bonjuer", "Chow"]
+print(list_1)
+del list_1[2]
+print(list_1)
+
+
+list_2 = [random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10), random.randint(0,10), random.randint(0,10)]
+print(list_2)
+if list_2[0] % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+if list_2[1] % 2 == 0:
+    print('Even')
+else:
+    print("Odd")
+if list_2[2] % 2 == 0:
+    print("Even")
+else:
+    print('Odd')
+if list_2[3] % 2 == 0:
+    print("Even")
+else:
+    print('Odd')
+if list_2[4] % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+if list_2[5] % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+gwood = "Chrizzzz"
+for x in gwood:
+    print(x)
+
+
+string = "Hello World"
+reverse_string = string[::-1]
+print(reverse_string)
+
+sentance = "You're a wizard Harry."
+
+
+newlist =[]
+for s in sentance:
+    if s == "e":
+        newlist.append("3")
+    else:
+        newlist.append(s)
+newlist
+print(newlist)
