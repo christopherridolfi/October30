fc796d7436183f5b1c5ab6e51cb1dffae2eabe1a
Christopher <christopher.ridolfi@greenwoodcollege.com>
2018-11-16 13:56:20 -0500

    Project

--- List/PBL Work Period.py
+++ List/PBL Work Period.py
@@ -22,9 +22,9 @@ shopping()
 
 
 
-row_1 = [1,2,3]
-row_2 = [4,5,6]
-row_3 = [7,8,9]
+row_1 = [0,0,0]
+row_2 = [0,0,0]
+row_3 = [0,0,0]
 
 tic_tac = [row_1, row_2,row_3]
  #First num is row, second is columb
@@ -35,51 +35,96 @@ while j == 0:
         j=1
     else:
         while t == 0:
-            place = input("Where Do you Want To Place Your X (tr = Top Right) (br = Bottom Right) (mm = Middle Middle")
             for x in tic_tac:
                 print(x)
+            place = input("Where Do you Want To Place Your X (tr = Top Right) (br = Bottom Right) (mm = Middle Middle")
+            player2 = input("Where Does player 2 to want to place X")
+
 
             if place == "tr":
-                tic_tac[0][2] = 0
+                tic_tac[0][2] = 1
             elif place == "br":
-                tic_tac[2][2] = 0
+                tic_tac[2][2] = 1
             elif place == "bl":
-                tic_tac[2][0] = 0
+                tic_tac[2][0] = 1
             elif place == "bm":
-                tic_tac[2][1] = 0
+                tic_tac[2][1] = 1
             elif place == "tm":
-                tic_tac[0][1] = 0
+                tic_tac[0][1] = 1
             elif place == "tl":
-                tic_tac[0][0] = 0
+                tic_tac[0][0] = 1
             elif place == "mr":
-                tic_tac[1][2] = 0
+                tic_tac[1][2] = 1
             elif place == "mm":
-                tic_tac[1][1] = 0
+                tic_tac[1][1] = 1
             elif place == "ml":
-                tic_tac[1][0] = 0
-            if tic_tac[0][0] + tic_tac[0][2] + tic_tac[0][1] == 0:
+                tic_tac[1][0] = 1
+            if tic_tac[0][0] + tic_tac[0][2] + tic_tac[0][1] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[1][0] + tic_tac[1][1] + tic_tac[1][2] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[2][2] + tic_tac[2][0] + tic_tac[2][1] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[2][0] + tic_tac[1][1] + tic_tac[0][2] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[2][2] + tic_tac[1][2] + tic_tac[0][2] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[2][1] + tic_tac[1][1] + tic_tac[0][1] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[2][0] + tic_tac[1][0] + tic_tac[0][0] == 3:
+                print("Player 1 Win")
+                t = t+1
+            if tic_tac[0][0] + tic_tac[1][1] + tic_tac[2][2] ==3:
+                print("Player 1 Win")
+                t = t+1
+
+            if player2 == "tr":
+                tic_tac[0][2] = 2.1
+            elif player2 == "br":
+                tic_tac[2][2] = 2.1
+            elif player2 == "bl":
+                tic_tac[2][0] = 2.1
+            elif player2 == "bm":
+                tic_tac[2][1] = 2.1
+            elif player2 == "tm":
+                tic_tac[0][1] = 2.1
+            elif player2 == "tl":
+                tic_tac[0][0] = 2.1
+            elif player2 == "mr":
+                tic_tac[1][2] = 2.1
+            elif player2 == "mm":
+                tic_tac[1][1] = 2.1
+            elif player2 == "ml":
+                tic_tac[1][0] = 2.1
+            if tic_tac[0][0] + tic_tac[0][2] + tic_tac[0][1] == 6.3:
+                print("Player 2 Wins")
+                t = t+1
+            if tic_tac[1][0] + tic_tac[1][1] + tic_tac[1][2] == 6.3:
+                print("Player 2 Wins")
+                t = t+1
+            if tic_tac[2][2] + tic_tac[2][0] + tic_tac[2][1] == 6.3:
+                print("Player 2 Wins")
+                t = t+1
+            if tic_tac[2][0] + tic_tac[1][1] + tic_tac[0][2] == 6.3:
+                print("Player 2 Wins")
+                t = t+1
+            if tic_tac[2][2] + tic_tac[1][2] + tic_tac[0][2] == 6.3:
                 print("You Win")
                 t = t+1
-            if tic_tac[1][0] + tic_tac[1][1] + tic_tac[1][2] == 0:
-                print("You Win")
+            if tic_tac[2][1] + tic_tac[1][1] + tic_tac[0][1] == 6.3:
+                print("Player 2 Wins")
                 t = t+1
-            if tic_tac[2][2] + tic_tac[2][0] + tic_tac[2][1] == 0:
-                print("You Win")
+            if tic_tac[2][0] + tic_tac[1][0] + tic_tac[0][0] == 6.3:
+                print("Player 2 Wins")
                 t = t+1
-            if tic_tac[2][0] + tic_tac[1][1] + tic_tac[0][2] == 0:
-                print("You Win")
-                t = t+1
-            if tic_tac[2][2] + tic_tac[1][2] + tic_tac[0][2] == 0:
-                print("You Win")
-                t = t+1
-            if tic_tac[2][1] + tic_tac[1][1] + tic_tac[0][1] == 0:
-                print("You Win")
-                t = t+1
-            if tic_tac[2][0] + tic_tac[1][0] + tic_tac[0][0] == 0:
-                print("You Win")
-                t = t+1
-            if tic_tac[0][0] + tic_tac[1][1] + tic_tac[2][2] ==0:
-                print("You Win")
+            if tic_tac[0][0] + tic_tac[1][1] + tic_tac[2][2] ==6.3:
+                print("Player 2 Wins")
                 t = t+1
 
 
@@ -103,6 +148,22 @@ for x in range(len(list3)):
     max1.insert(0, max(list3))
     list3.remove(max(list3))
     print(max1)
+
+
+
+innlistt = [2,7,3,4,9]
+def pivotlist(innlistt,number):
+    newwlist = []
+    for x in innlistt:
+        if x < number:
+            newwlist.append(x)
+    return newwlist
+
+print(pivotlist(innlistt,6))
+
+
+
+
 
 
 
